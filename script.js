d63df74e23fd0809b0565cfacb4f0c516024dfdf
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-07-30 17:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,29 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
-		    let result = []
-	    for(i=0; i<arr.length; i++){
-	         if(arr[i] + arr[i+1] <= n){
-	               result.push(arr.slice(i,i+1))
-	               i++;
-	         }
-	         else{
-	              result.push(arr.slice(i))
-	         }
-	         return result
-	    }
+		let result = [];
+    let currentChunk = [];
+    let currentSum = 0;
 
+    for (let num of arr) {
+        // Check if adding the current number would exceed the max sum
+        if (currentSum + num <= n) {
+            currentChunk.push(num);
+            currentSum += num;
+        } else {
+            // If it exceeds, add the current chunk to the result and start a new chunk
+            result.push(currentChunk);
+            currentChunk = [num];
+            currentSum = num;
+        }
+    }
+
+    // Add the last chunk to the result if it's not empty
+    if (currentChunk.length > 0) {
+        result.push(currentChunk);
+    }
+
+    return result;
 };
 
 const n = prompt("Enter n: ");
