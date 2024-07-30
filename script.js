7ef674c8de3cb19014ca05c1ccaa359f8c7caf15
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-07-30 17:32:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+		    let result = []
+	    for(i=0; i<arr.length; i++){
+	         if(arr[i] + arr[i+1] <= n){
+	               result.push(arr.slice(i,i+1))
+	               i++;
+	         }
+	         else{
+	              result.push(arr.slice(i))
+	         }
+	         return result
+	    }
+
 };
 
 const n = prompt("Enter n: ");
